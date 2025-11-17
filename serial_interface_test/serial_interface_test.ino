53eed654401b5f8f82534713349ee7110aa9a44a
zach-vanzura <zachvanzura@gmail.com>
2025-11-17 09:55:38 -0500

    commit so i can pull

--- serial_interface_test/serial_interface_test.ino
+++ serial_interface_test/serial_interface_test.ino
@@ -1,9 +1,28 @@
+#include <BleKeyboard.h>
+
+/*
+This sketch is a test script for using Keyboard.h and Mouse.h to generate serial commands 
+to play minecraft
+
+Created by Zach Vanzura in collaboration with Owen Cook
+11/14/2025
+*/
+
+
+#define LED_BUILTIN 2
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 
 }
